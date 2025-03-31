2b1fa131077794677c204a5ced4cdd5303d9c3a7
AmitPathak <amit7081483564@gmail.com>
2025-03-31 17:53:38 +0530

    implement user authentication with login and profile functionality, including error handling and validation

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,6 @@ Route::get('/',[HomeController::class, 'index'])->name('home');
 Route::get('/account/register',[AccountController::class, 'registration'])->name('account.registration');
 Route::post('/account/process-register',[AccountController::class, 'processRegister'])->name('account.processRegister');
 Route::get('/account/login',[AccountController::class, 'login'])->name('account.login');
+
+Route::post('/account/authenticate',[AccountController::class, 'authenticate'])->name('account.authenticate');
+Route::get('/account/profile',[AccountController::class, 'profile'])->name('account.profile');
