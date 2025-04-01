c6c393ed0cc4fa4ef6b504a92706d6ba485e4ad1
AmitPathak <amit7081483564@gmail.com>
2025-04-01 12:43:53 +0530

    add profile update functionality with validation and success/error handling

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -66,22 +66,61 @@ class AccountController extends Controller
 
             if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
                 return redirect()->route('account.profile');
-                
             } else {
                 return redirect()->route('account.login')->with('error', 'Either Email/Password is incorrect!');
             }
-            
-
         } else {
             return redirect()->route('account.login')->withErrors($validator)->withInput($request->only('email'));
         }
     }
 
-    public function profile(){
-        return view('front.account.profile');
+    public function profile()
+    {
+
+        $id = Auth::user()->id;
+        $user = User::where('id', $id)->first();
+
+        return view('front.account.profile', [
+            'user' => $user
+        ]);
     }
-    
-    public function logout(){
+
+    public function updateProfile(Request $request)
+    {
+        $id = Auth::user()->id;
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:3',
+            'email' => 'required|email|unique:users,email,' . $id . ',id',
+        ]);
+
+        if ($validator->passes()) {
+            
+            $user  = User::find($id);
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->designation = $request->designation;
+            $user->mobile = $request->mobile;
+            $user->save();
+
+            session()->flash('success', 'Your Profile Update successfully.');
+
+            return response()->json([
+                'status' => true,
+                'errors' => [],
+            ]);
+
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors(),
+            ]);
+        }
+        
+    }
+
+
+    public function logout()
+    {
         Auth::logout();
         return redirect()->route('account.login');
     }
